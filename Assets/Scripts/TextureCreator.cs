66784a88b3b7269d8115f6ef02e4ca2920c38110
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-05-31 18:00:54 +0800

    create texture

---
+++ Assets/Scripts/TextureCreator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCreator : MonoBehaviour
+{
+    [Range(2, 512)] public int resolution = 256;
+    private Texture2D texture;
+
+    private void OnEnable()
+    {
+        texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, false);
+        texture.name = "Procedural Texture";
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.filterMode = FilterMode.Bilinear;
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+        FillTexture();
+    }
+
+    public void FillTexture()
+    {
+        if (texture.width != resolution)
+        {
+            texture.Resize(resolution, resolution);
+        }
+
+        float stepSize = 1f / resolution;
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                texture.SetPixel(x, y,
+                    new Color((x + 0.5f) * stepSize % 0.1f, (y + 0.5f) * stepSize % 0.1f, 0f) * 10f);
+            }
+        }
+
+        texture.Apply();
+    }
+}
